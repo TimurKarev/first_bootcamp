379715e899061fad4a4c6ff7b8ef834bdd96be1a
timurkarev <timurkarev@mail.ru>
2022-04-16 21:37:13 +0300

    feat(screens/colored_list): create colored_list_page.dart

--- lib/screens/colored_list/colored_list_page.dart
+++ lib/screens/colored_list/colored_list_page.dart
@@ -1,14 +1,46 @@
 import 'package:first_bootcamp/assets/strings/strings_app.dart';
+import 'package:first_bootcamp/screens/colored_list/widgets/list_tile_app.dart';
+import 'package:first_bootcamp/theme/colors_app.dart';
+import 'package:first_bootcamp/utils/utils_app.dart';
 import 'package:flutter/material.dart';
 
-class ColoredListPage extends StatelessWidget {
+class ColoredListPage extends StatefulWidget {
   const ColoredListPage({Key? key}) : super(key: key);
 
+  @override
+  State<ColoredListPage> createState() => _ColoredListPageState();
+}
+
+class _ColoredListPageState extends State<ColoredListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(StringsApp.mainScreenTaskItemSecondText),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 9.0,
+        ),
+        child: ListView.builder(
+          itemCount: 1000,
+          itemBuilder: (context, index) => ListTileApp(
+            color: UtilsApp.getRandomColor(),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {});
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: const Icon(
+          Icons.edit,
+          color: ColorsApp.black,
+        ),
       ),
     );
   }
